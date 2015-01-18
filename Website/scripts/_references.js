e0f957657f6df3053f1d182e8b1c6c5c69c035a9
Arun Endapally <arun.e.kumar@gmail.com>
2015-01-18 16:03:06 +0530

    minifications & changes to search

--- Website/scripts/_references.js
+++ Website/scripts/_references.js
@@ -4,3 +4,5 @@
 /// <reference path="comments.js" />
 /// <reference path="jquery-2.0.2.js" />
 /// <reference path="googleanalytics.js" />
+/// <reference path="google-search.js" />
+/// <reference path="jquery-2.0.2.intellisense.min.js" />
---
+++ Website/scripts/google-search.js
@@ -0,0 +1,10 @@
+﻿(function () {
+  var cx = '000692877211149048631:q1akl3gc2-y';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+      '//www.google.com/cse/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
